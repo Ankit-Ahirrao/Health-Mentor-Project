4b4d4bbf6a7d77e75b130ab595a3026118dbdab3
Ankit Ahirrao <43294109+Ankit-Ahirrao@users.noreply.github.com>
2020-07-21 17:14:47 +0530

    Add blog apps files

---
+++ urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
+    path('post/<id>/', views.post, name='post-detail'),
+    
+    path('post/<int:pk>/edit/',
+        views.BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/',
+        views.BlogDeleteView.as_view(), name='post_delete'),
+]
